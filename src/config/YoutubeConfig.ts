32de47da5c465f15bdb5e9a9852aacb8ef9e5b92
Andriy <andsei91@gmail.com>
2019-12-15 13:23:22 +0100

    update to 1.5

--- src/config/YoutubeConfig.ts
+++
@@ -1,7 +0,0 @@
-import * as env from 'env-var';
-import { config as envConfig } from 'dotenv';
-envConfig();
-
-export class YoutubeConfig {
-    public static readonly YOUTUBE_API_KEY = env.get('YOUTUBE_API_KEY').asString();
-}
