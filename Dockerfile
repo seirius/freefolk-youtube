2b2a544da0255654d6e362077bf6867a123ed992
Andriy <andsei91@gmail.com>
2020-01-03 09:40:41 +0100

    update: 1.0.28

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:12.13.1-alpine AS build
 
 WORKDIR /usr/src/app
 
+RUN apk add git
+
 COPY ./ ./
 
 RUN npm install && npm install -g @nestjs/cli && nest build
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,36 @@
-import { Module } from '@nestjs/common';
-import { DefaultModule } from './default/default.module';
-import { YoutubeModule } from './youtube/youtube.module';
+import { Module, Logger } from "@nestjs/common";
+import { DefaultModule } from "./default/default.module";
+import { YoutubeModule } from "./youtube/youtube.module";
+import { ProcessStateModule, ProcessStateService, EProcessState } from "nest-mqtt-client";
 
 @Module({
     imports: [
         DefaultModule,
-        YoutubeModule
+        YoutubeModule,
+        ProcessStateModule,
     ],
     controllers: [],
     providers: [],
 })
-export class AppModule { }
+export class AppModule {
+    constructor(
+        private readonly processStateService: ProcessStateService,
+    ) {
+        this.initProcessStateListener();
+    }
+
+    private initProcessStateListener(): void {
+        this.processStateService.onReportQuestion()
+        .subscribe(({ petitionId }) => {
+            new Logger().log(petitionId);
+            this.processStateService.sendReport({
+                name: "freefolk-youtube",
+                petitionId,
+                state: EProcessState.OK,
+                info: {
+                    ok: "notOk",
+                },
+            });
+        });
+    }
+}
